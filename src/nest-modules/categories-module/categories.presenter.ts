d6f4712d756f262e68ce7fa89c65c92305d20979
Paulo Sarmento <paulo.cesar.sarmento@hotmail.com>
2024-02-07 16:26:23 -0300

    Fechando testes de integracao do controller de categoria

--- src/nest-modules/categories-module/categories.presenter.ts
+++ src/nest-modules/categories-module/categories.presenter.ts
@@ -1,12 +1,13 @@
 import { Transform } from 'class-transformer';
 import { CategoryOutput } from '../../core/category/application/use-cases/common/category-output';
-import { ListCategoriesOutput } from '@core/category/application/use-cases/list-categories/list-categories.use-case';
+import { ListCategoriesOutput } from '../../core/category/application/use-cases/list-categories/list-categories.use-case';
 import { CollectionPresenter } from '../shared-module/collection.presenter';
 
 export class CategoryPresenter {
   id: string;
   name: string;
   description: string | null;
+  is_active: boolean;
   @Transform(({ value }: { value: Date }) => value.toISOString())
   created_at: Date;
 
@@ -14,6 +15,7 @@ export class CategoryPresenter {
     this.id = output.id;
     this.name = output.name;
     this.description = output.description;
+    this.is_active = output.is_active;
     this.created_at = output.created_at;
   }
 }
