d8be0b06f2e6b34f9fe1ab9f46116b764abb5a7c
Paulo Sarmento <paulo.cesar.sarmento@hotmail.com>
2024-02-15 17:19:38 -0300

    Criando modulo Nest.js do vídeo

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { CategoriesModule } from './nest-modules/categories-module/categories.mo
 import { SharedModule } from './nest-modules/shared-module/shared.module';
 import { CastMembersModule } from './nest-modules/cast-members-module/cast-members.module';
 import { GenresModule } from './nest-modules/genres-module/genres.module';
+import { VideosModule } from './nest-modules/videos-module/videos.module';
 
 @Module({
   imports: [
@@ -13,7 +14,8 @@ import { GenresModule } from './nest-modules/genres-module/genres.module';
     CategoriesModule,
     CastMembersModule,
     SharedModule,
-    GenresModule
+    GenresModule,
+    VideosModule
   ],
 })
 export class AppModule {}
