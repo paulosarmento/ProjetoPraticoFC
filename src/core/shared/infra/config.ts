6795639b59d5865edea2fce2a4d45339556866a5
Paulo Sarmento <paulo.cesar.sarmento@hotmail.com>
2024-02-14 23:47:24 -0300

    Criando servico para administrar uploads no Google Cloud Storage

--- src/core/shared/infra/config.ts
+++ src/core/shared/infra/config.ts
@@ -13,6 +13,17 @@ export class Config {
       logging: Config.env.DB_LOGGING === 'true',
     };
   }
+  static bucketName() {
+    Config.readEnv();
+
+    return Config.env.GOOGLE_CLOUD_STORAGE_BUCKET_NAME;
+  }
+
+  static googleCredentials() {
+    Config.readEnv();
+
+    return JSON.parse(Config.env.GOOGLE_CLOUD_CREDENTIALS);
+  }
 
   static readEnv() {
     if (Config.env) {
